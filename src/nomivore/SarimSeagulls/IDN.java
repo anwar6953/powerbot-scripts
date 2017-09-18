5668b41bcfe50f5173483a9f04d247319690d4c0
nomivore <vincent.tan@outlook.com.au>
2017-09-18 10:17:29 +1000

    using source folder

---
+++ src/nomivore/SarimSeagulls/IDN.java
@@ -0,0 +1,68 @@
+package nomivore.SarimSeagulls;
+
+public class IDN {
+    // Monsters
+    public static final int
+            RAT_GIANT = 3313;
+
+    public static final int[]
+            CHICKEN = {2693,2692,2819,2820},
+            SEAGULL = {1338,1339};
+
+    //ITEMS
+    public static final int
+            BONES = 526,
+            FEATHER = 314,
+            CHICKEN_RAW = 2138,
+            BUCKET = 1925,
+            EGG = 1944,
+            LOGS_NORMAL = 1511,
+            POT_FLOUR = 1933,
+            JUG_FULL = 1937,
+            PIE_DISH = 2313,
+            DOUGH_PASTRY = 1953;
+
+    // TOOLS
+    public static final int
+            FISHING_ROD_FLY = 309,
+            FISHING_NET = 303,
+            TINDERBOX = 590;
+
+    //COOKED FOOD
+    public static final int
+    TROUT = 333,
+    SALMON = 329;
+
+    // RAW FOOD
+    public static final int
+            RAW_CHICKEN = 2138,
+            RAW_BEEF = 2132,
+            RAW_ANCHOVY = 321,
+            RAW_SHRIMP = 317,
+            RAW_SARDINE = 327,
+            RAW_SALMON = 331,
+            RAW_TROUT = 335,
+            RAW_COD =341,
+            RAW_HERRING = 345,
+            RAW_PIKE = 349,
+            RAW_TUNA = 359,
+            RAW_BASS = 363,
+            RAW_LOBSTER = 377,
+            RAW_SWORDFISH = 371;
+
+    //OBJECTS
+    public static final int
+            FIRE = 26185;
+
+    //WIDGETS
+    public static final int MAGIC_WIDGET = 218,
+            ENCHANT_BOLT_SPELL = 4,
+            ENCHANT_BOLT_WIDGET = 80,
+            ENCHANT_RUBY_BOLT = 8,
+            ENCHANT_DIAMOND_SPELL = 37,
+            SPELL_SPIN_FLAX = 138,
+            WIDGET_CHATBOX = 162,
+            WIDGET_MAKE = 547,
+            SPELL_HIGH_ALCH = 35;
+
+}
